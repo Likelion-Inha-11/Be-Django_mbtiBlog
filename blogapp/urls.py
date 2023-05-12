e60d64ded7470b043d909041aa751e86dc82a825
donghapark <donghaha12@naver.com>
2023-05-12 18:54:42 +0900

    댓글

---
+++ blogapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('mbtitest/', views.mbtitest, name='mbtitest'),
+    path('mbtiresult/', views.mbtiresult, name='mbtiresult'),
+ 
+]
